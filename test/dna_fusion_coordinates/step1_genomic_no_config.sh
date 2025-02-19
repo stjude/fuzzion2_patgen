b5d80945bc29082ba20ba6695fd8006c2dcd4ce4
Edmonson <Michael.Edmonson@stjude.org>
2025-02-19 11:46:39 -0500

    add test/dna_fusion_coordinates

---
+++ test/dna_fusion_coordinates/step1_genomic_no_config.sh
@@ -0,0 +1,2 @@
+fusion_contig_extension.pl -genome GRCh37-lite -genomic -crest -file test.tsv -annotate-genes-as-breakpoints -fasta GRCh37-lite.fa -refflat ncbiRefSeq.txt
+
---
+++ test/dna_fusion_coordinates/step2_convert.sh
@@ -0,0 +1,1 @@
+fusion_contig_extension.pl -convert-fuzzion test.tsv.extended.tab -no-blacklist -cicero
